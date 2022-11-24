96c99c4939d4b28ec4e37399d5215bb2e295ed99
Kyrre Gjerstad <kyrregjerstad@gmail.com>
2022-11-24 22:14:41 +0100

    feat add parallax animation on scroll

---
+++ app/script.js
@@ -0,0 +1,71 @@
+gsap.registerPlugin(ScrollTrigger);
+
+let timeline = gsap.timeline();
+
+timeline.to("scroll-anim-a", {
+  scrollTrigger: {
+    trigger: "scroll-anim-a",
+    start: "top center",
+    scrub: 3,
+  },
+  duration: 5,
+  rotation: 10,
+  scale: 1.5,
+});
+
+timeline.to(".scroll-anim-b", {
+  scrollTrigger: {
+    trigger: ".scroll-anim-b",
+    start: "top center",
+    scrub: 3,
+  },
+  duration: 5,
+  scale: 2,
+  y: -120,
+});
+
+timeline.to(".scroll-anim-c", {
+  scrollTrigger: {
+    trigger: ".scroll-anim-c",
+    start: "top center",
+    scrub: 3,
+  },
+  duration: 5,
+  scale: 1.1,
+  x: -20,
+  // y: -120,
+});
+timeline.to(".scroll-anim-d", {
+  scrollTrigger: {
+    trigger: ".scroll-anim-d",
+    start: "top center",
+    scrub: 3,
+  },
+  duration: 5,
+  scale: 1.08,
+  // x: 10,
+  // y: -120,
+});
+
+// timeline.from(".card-background", {
+//   scrollTrigger: {
+//     trigger: ".card-background",
+//     toggleActions: "restart none none none",
+//   },
+//   duration: 5,
+//   rotation: 15,
+//   scale: 1.5,
+// });
+// timeline.to(
+//   ".card-foreground",
+//   {
+//     scrollTrigger: {
+//       trigger: ".card-background",
+//       toggleActions: "restart none none none",
+//     },
+//     duration: 5,
+//     scale: 2,
+//     y: -100,
+//   },
+//   0
+// );
