6b29a3f9b70fd586a690be8b7bdccb30d026ea0f
Kyrre Gjerstad <kyrregjerstad@gmail.com>
2022-12-04 20:09:39 +0100

    feat add sidebar

--- app/script.js
+++ app/script.js
@@ -128,6 +128,7 @@ ScrollTrigger.batch(".card-teaser-text", {
 });
 
 const hamburgerMenu = document.querySelector("#hamburger-button");
+const sidebar = document.querySelector("#sidebar");
 
 hamburgerMenu.addEventListener("click", () => {
   const currentState = hamburgerMenu.getAttribute("data-state");
@@ -135,8 +136,10 @@ hamburgerMenu.addEventListener("click", () => {
   if (!currentState || currentState === "closed") {
     hamburgerMenu.setAttribute("data-state", "opened");
     hamburgerMenu.setAttribute("aria-expanded", "true");
+    sidebar.style.translate = "0%";
   } else {
     hamburgerMenu.setAttribute("data-state", "closed");
     hamburgerMenu.setAttribute("aria-expanded", "false");
+    sidebar.style.translate = "100%";
   }
 });
