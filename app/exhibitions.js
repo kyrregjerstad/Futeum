1a6b06b26fdbcc61f4ab08a63739203b212fa3f2
Kyrre Gjerstad <kyrregjerstad@gmail.com>
2022-12-06 18:09:11 +0100

    update exhibitions page

---
+++ app/exhibitions.js
@@ -0,0 +1,50 @@
+gsap.registerPlugin(ScrollTrigger);
+
+let timeline = gsap.timeline();
+
+timeline.to(".scroll-anim-a", {
+  scrollTrigger: {
+    trigger: ".scroll-anim-a",
+    start: "top center",
+    scrub: 3,
+  },
+  duration: 5,
+  rotation: 5,
+  scale: 1.25,
+  y: -50,
+});
+
+timeline.to(".scroll-anim-b", {
+  scrollTrigger: {
+    trigger: ".scroll-anim-b",
+    start: "top center",
+    scrub: 3,
+  },
+  duration: 5,
+  scale: 1.5,
+  rotation: -15,
+  y: -60,
+});
+
+timeline.to(".scroll-anim-c", {
+  scrollTrigger: {
+    trigger: ".scroll-anim-c",
+    start: "top center",
+    scrub: 3,
+  },
+  duration: 5,
+  scale: 1.1,
+  x: -20,
+  // y: -120,
+});
+timeline.to(".scroll-anim-d", {
+  scrollTrigger: {
+    trigger: ".scroll-anim-d",
+    start: "top center",
+    scrub: 3,
+  },
+  duration: 5,
+  scale: 1.03,
+  // x: 10,
+  // y: -120,
+});
